fd4d0671db765073204ca361da64e34bb949b2a9
Aruchan THAMILCHELVAN <aruchantha@gmail.com>
2026-09-22 12:46:40 +0200

    server_part

--- ft_irc/srcs/main.cpp
+++ ft_irc/srcs/main.cpp
@@ -3,70 +3,105 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: athamilc <athamilc@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/22 19:05:02 by athamilc          #+#    #+#             */
-/*   Updated: 2026/08/22 19:05:14 by athamilc         ###   ########.fr       */
+/*   Updated: 2026/09/22 12:34:02 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Server.hpp"
+#include "../includes/Server.hpp"
 #include <iostream>
 #include <cstdlib>   // Pour atoi
 #include <csignal>   // Pour sigaction, SIGINT
 
-Server *g_serverPtr = NULL;
-
-void handleSignal(int signum)
+int main(int c, char **v)
 {
-    (void)signum;
-    std::cout << "\n[Server] Signal d'arrêt reçu. Fermeture propre..." << std::endl;
-    if (g_serverPtr)
-        g_serverPtr->stop();
-}
-
-int main(int argc, char **argv)
-{
-    if (argc != 3)
+	if (c != 3)
     {
         std::cerr << "Usage: ./ircserv <port> <password>" << std::endl;
         return 1;
     }
 
-    int port = atoi(argv[1]); // <- Sans 'std::'
+    int port = atoi(v[1]);
     if (port <= 1024 || port > 65535)
     {
         std::cerr << "Erreur: Port invalide (utiliser une valeur entre 1025 et 65535)." << std::endl;
         return 1;
     }
-
-    std::string password = argv[2];
+    std::string password = v[2];
     if (password.empty())
     {
         std::cerr << "Erreur: Le mot de passe ne peut pas être vide." << std::endl;
         return 1;
     }
-
-    // Gestion du signal SIGINT (Ctrl+C)
-    struct sigaction sa;
-    sa.sa_handler = handleSignal;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
-    sigaction(SIGINT, &sa, NULL);
-
-    try
-    {
-        Server server(port, password);
-        g_serverPtr = &server;
-
-        server.initNetwork();
-        server.startLoop();
-    }
-    catch (const std::exception &e)
-    {
-        std::cerr << e.what() << std::endl;
-        return 1;
-    }
-
-    return 0;
+	try
+	{
+		Server Server(v[1], v[2]);
+		addrinfo *res = Server.init_server(v[1]);
+		Server.startLoop();
+		freeaddrinfo(res);
+	}
+	catch(const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+		return(1);
+	}
+	return(0);
 }
+
+// Server *g_serverPtr = NULL;
+
+// void handleSignal(int signum)
+// {
+//     (void)signum;
+//     std::cout << "\n[Server] Signal d'arrêt reçu. Fermeture propre..." << std::endl;
+//     if (g_serverPtr)
+//         g_serverPtr->stop();
+// }
+
+// int main(int argc, char **argv)
+// {
+//     if (argc != 3)
+//     {
+//         std::cerr << "Usage: ./ircserv <port> <password>" << std::endl;
+//         return 1;
+//     }
+
+//     int port = atoi(argv[1]); // <- Sans 'std::'
+//     if (port <= 1024 || port > 65535)
+//     {
+//         std::cerr << "Erreur: Port invalide (utiliser une valeur entre 1025 et 65535)." << std::endl;
+//         return 1;
+//     }
+
+//     std::string password = argv[2];
+//     if (password.empty())
+//     {
+//         std::cerr << "Erreur: Le mot de passe ne peut pas être vide." << std::endl;
+//         return 1;
+//     }
+
+//     // Gestion du signal SIGINT (Ctrl+C)
+//     struct sigaction sa;
+//     sa.sa_handler = handleSignal;
+//     sigemptyset(&sa.sa_mask);
+//     sa.sa_flags = 0;
+//     sigaction(SIGINT, &sa, NULL);
+
+//     try
+//     {
+//         Server server(port, password);
+//         g_serverPtr = &server;
+
+//         server.initNetwork();
+//         server.startLoop();
+//     }
+//     catch (const std::exception &e)
+//     {
+//         std::cerr << e.what() << std::endl;
+//         return 1;
+//     }
+
+//     return 0;
+// }
--- ft_irc/srcs/server/Server.cpp
+++ ft_irc/srcs/server/Server.cpp
@@ -3,221 +3,227 @@
 /*                                                        :::      ::::::::   */
 /*   Server.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yabou-da <yabou-da@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/20 15:11:29 by yabou-da          #+#    #+#             */
-/*   Updated: 2026/08/17 19:26:03 by yabou-da         ###   ########.fr       */
+/*   Updated: 2026/09/22 12:34:59 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Server.hpp"
 
-Server::Server(int port, const std::string &password)
-    : _port(port), _password(password), _socketFd(-1), _running(false)
-{
-}
+Server::Server(std::string port, std::string password) : _port(port), _password(password), _socketFd(-1){}
 
 Server::Server(const Server &obj)
 {
-    *this = obj;
+	*this=obj;
 }
 
 Server &Server::operator=(const Server &obj)
 {
-    if (this != &obj)
-    {
-        this->_port = obj._port;
-        this->_password = obj._password;
-        this->_socketFd = obj._socketFd;
-        this->_running = obj._running;
-        this->_pollVec = obj._pollVec;
-    }
-    return *this;
+	if(this != &obj)
+	{
+		this->_port = obj._port;
+		this->_password = obj._password;
+		this->_socketFd = obj._socketFd;
+		this->_pollVec= obj._pollVec;
+		// this->_clients = obj._clients;
+		// this->_channel = obj._channel;
+	}
+	return(*this);
 }
 
-Server::~Server()
+Server::~Server(){}
+
+addrinfo *Server::init_server(const char *port)
 {
-    // Fermeture de toutes les sockets ouvertes (clients + écoute)
-    for (size_t i = 0; i < this->_pollVec.size(); ++i)
+	int status;
+	struct addrinfo hints;
+	struct addrinfo *res;
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+
+	// part 1 : getaddrinfo() to prepare all the structs we need
+	status = getaddrinfo(NULL, port, &hints, &res);
+	if (status != 0)
+	{
+		std::cout<<"getaddrinfo : failed" << std::endl;
+		throw Server::initNetworkException();
+	}
+	std::cout<<"getaddrinfo: OK! (Port : "<< port <<")"<< std::endl;
+
+	// part 2 : socket() to open a socket fd for listening 
+	this->_socketFd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	if(this->_socketFd == -1)
+	{
+		std::cout<<"getaddrinfo : failed" << std::endl;
+		freeaddrinfo(res);
+		close(this->_socketFd);
+		throw Server::initNetworkException();
+	}
+	std::cout<<"listening socket creation : OK! (fd : "<<_socketFd<<")"<< std::endl;
+
+	// part 2.5 : setsockopt() to to configure socket_fd
+	int opt = 1;
+	status = setsockopt(this->_socketFd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(int));
+	if(status == -1)
+	{
+		std::cout<<"setsockopt: failed" << std::endl;
+		freeaddrinfo(res);
+		close(this->_socketFd);
+		throw Server::initNetworkException();
+	}
+	if (fcntl(this->_socketFd, F_SETFL, O_NONBLOCK) == -1)
     {
-        if (this->_pollVec[i].fd >= 0)
+        std::cout << "fcntl O_NONBLOCK : failed" << std::endl;
+        freeaddrinfo(res);
+        close(this->_socketFd);
+        throw Server::initNetworkException();
+    }
+	std::cout<<"option socket setup : OK!"<< std::endl;
+
+	// part 3 : bind() to link socket_fd and port
+	status = bind(this->_socketFd, res->ai_addr, res->ai_addrlen);
+	if(status == -1)
+	{
+		std::cout<<"bind: failed" << std::endl;
+		freeaddrinfo(res);
+		close(this->_socketFd);
+		throw Server::initNetworkException();
+	}
+	std::cout<<"bind : OK!"<< std::endl;
+
+	//part 4 : listen() to wait for entering connection
+	status = listen(this->_socketFd, 20);
+	if(status == -1)
+	{
+		std::cout<<"listen : failed" << std::endl;
+		freeaddrinfo(res);
+		close(this->_socketFd);
+		throw Server::initNetworkException();
+	}
+	std::cout<<"listen : OK!"<< std::endl;
+	freeaddrinfo(res);
+	return(res);
+}
+
+void Server::disconnectClient(size_t i)
+{
+	int clientFd = this->_pollVec[i].fd;
+	close(clientFd);
+	this->_pollVec.erase(this->_pollVec.begin() + i);
+	std::cout << "Client on fd " << clientFd << " disconnected." << std::endl;
+}
+
+void Server::acceptNewClient()
+{
+	struct sockaddr_in clientAddr;
+	socklen_t clientLen = sizeof(clientAddr);
+
+	int newClient = accept(this->_socketFd, (struct sockaddr *)&clientAddr, &clientLen);
+	if (newClient == -1)
+	{
+		std::cout<<"accept : failed" << std::endl;
+		// attention au valgrind
+		throw Server::initNetworkException();
+	}
+	if (fcntl(newClient, F_SETFL, O_NONBLOCK) == -1)
+    {
+		std::cout<<"fcntl : failed" << std::endl;
+		// attention au valgrind
+		close(newClient);
+		throw Server::initNetworkException();
+    }
+	struct pollfd clientFd;
+	clientFd.fd = newClient;
+    clientFd.events = POLLIN;
+    clientFd.revents = 0;
+    this->_pollVec.push_back(clientFd);
+	std::cout << "New client accepted with fd " << newClient<< std::endl;
+}
+
+// Fonction a retravailler
+void Server::handleClientData(size_t i)
+{
+    char buffer[512];
+    int clientFd = this->_pollVec[i].fd;
+
+    ssize_t bytesRead = recv(clientFd, buffer, sizeof(buffer) - 1, 0);
+
+    // Cas d'erreur ou de fermeture de connexion par le client
+    if (bytesRead <= 0)
+    {
+        if (bytesRead == 0)
+            std::cout << "Le client " << clientFd << " a fermé la connexion." << std::endl;
+        else
+            perror("recv() failed");
+
+        this->disconnectClient(i);
+        return;
+    }
+
+    // Assurer la terminaison de la chaîne reçue
+    buffer[bytesRead] = '\0';
+    std::string receivedData(buffer);
+
+    // TODO: Ajouter à un buffer spécifique au client et découper par "\r\n"
+    // pour transmettre les commandes complètes à tes coéquipiers
+    std::cout << "Reçu du client " << clientFd << " : " << receivedData << std::endl;
+}
+
+void Server::startLoop(){
+	this->_pollVec.clear();
+
+	struct pollfd listen_fds;
+	listen_fds.fd = this->_socketFd;
+	listen_fds.events = POLLIN;
+	listen_fds.revents = 0;
+
+	this->_pollVec.push_back(listen_fds);
+	this->_running = true;
+    std::cout << "Waiting for connexions..." << std::endl;
+	while (this->_running)
+	{
+		int c = poll(&this->_pollVec[0], this->_pollVec.size(), -1);
+		if (c == -1)
+		{
+			if (this->_running == false)
+                break;
+			std::cout<<"pool: failed" << std::endl;
+			close(this->_socketFd);
+			throw Server::initNetworkException();
+		}
+		for (size_t i = 0; i < this->_pollVec.size(); ++i)
+		{
+			if (this->_pollVec[i].revents & (POLLHUP | POLLERR | POLLNVAL))
+			{
+				this->disconnectClient(i);
+				--i;
+				continue;
+			}
+			if (this->_pollVec[i].revents & POLLIN)
+			{
+				if (this->_pollVec[i].fd == this->_socketFd)
+				{
+					this->acceptNewClient();
+				}
+				else
+				{
+					size_t initialSize = this->_pollVec.size();
+					this->handleClientData(i);
+					if (this->_pollVec.size() < initialSize)
+                		--i;
+				}
+			}
+		}
+	}
+	for (size_t i = 0; i < this->_pollVec.size(); ++i)
+    {
+        if (this->_pollVec[i].fd != -1)
             close(this->_pollVec[i].fd);
     }
     this->_pollVec.clear();
-}
-
-void Server::initNetwork()
-{
-    // 1. Création de la socket IPv4 / TCP
-    this->_socketFd = socket(AF_INET, SOCK_STREAM, 0);
-    if (this->_socketFd == -1)
-        throw Server::SocketException();
-
-    // 2. Libération immédiate du port à l'arrêt du serveur
-    int opt = 1;
-    if (setsockopt(this->_socketFd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
-    {
-        close(this->_socketFd);
-        throw Server::SetsockoptException();
-    }
-
-    // 3. Passage en mode non-bloquant
-    int flags = fcntl(this->_socketFd, F_GETFL, 0);
-    if (flags == -1 || fcntl(this->_socketFd, F_SETFL, flags | O_NONBLOCK) == -1)
-    {
-        close(this->_socketFd);
-        throw Server::FcntlException();
-    }
-
-    // 4. Configuration de l'adresse et liaison (bind)
-    struct sockaddr_in serverAddr;
-    std::memset(&serverAddr, 0, sizeof(serverAddr));
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = INADDR_ANY; // Écoute sur toutes les interfaces réseau
-    serverAddr.sin_port = htons(this->_port); // Conversion en ordre d'octets réseau (Big-Endian)
-
-    if (bind(this->_socketFd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1)
-    {
-        close(this->_socketFd);
-        throw Server::BindException();
-    }
-
-    // 5. Activation du mode écoute passive
-    if (listen(this->_socketFd, SOMAXCONN) == -1)
-    {
-        close(this->_socketFd);
-        throw Server::ListenException();
-    }
-
-    std::cout << "[Server] Initialisation réussie sur le port " << this->_port << std::endl;
-}
-
-void Server::startLoop()
-{
-    // Enregistrement de la socket d'écoute dans poll
-    struct pollfd listenPollFd;
-    listenPollFd.fd = this->_socketFd;
-    listenPollFd.events = POLLIN;
-    listenPollFd.revents = 0;
-    this->_pollVec.push_back(listenPollFd);
-
-    this->_running = true;
-    std::cout << "[Server] Boucle événementielle lancée..." << std::endl;
-
-    while (this->_running)
-    {
-        int pollCount = poll(&this->_pollVec[0], this->_pollVec.size(), -1);
-        if (pollCount < 0)
-        {
-            if (this->_running)
-                std::cerr << "[Server] Erreur lors de poll()" << std::endl;
-            break;
-        }
-
-        // Parcours des descripteurs surveillés
-        for (size_t i = 0; i < this->_pollVec.size(); ++i)
-        {
-            // Vérification de la présence de données à lire
-            if (this->_pollVec[i].revents & POLLIN)
-            {
-                if (this->_pollVec[i].fd == this->_socketFd)
-                {
-                    // Nouvelle connexion entrante
-                    this->_acceptNewClient();
-                }
-                else
-                {
-                    // Données envoyées par un client existant
-                    this->_handleClientData(i);
-                }
-            }
-            // Gestion des erreurs / déconnexions matérielles
-            else if (this->_pollVec[i].revents & (POLLERR | POLLHUP | POLLNVAL))
-            {
-                if (this->_pollVec[i].fd != this->_socketFd)
-                {
-                    this->_disconnectClient(i);
-                    --i; // Ajustement de l'index suite au redimensionnement du vecteur
-                }
-            }
-        }
-    }
-}
-
-void Server::_acceptNewClient()
-{
-    struct sockaddr_in clientAddr;
-    socklen_t clientLen = sizeof(clientAddr);
-
-    int clientFd = accept(this->_socketFd, (struct sockaddr *)&clientAddr, &clientLen);
-    if (clientFd == -1)
-    {
-        std::cerr << "[Server] Échec de l'appel à accept()" << std::endl;
-        return;
-    }
-
-    // Passage obligatoire de la socket client en mode non-bloquant
-    int flags = fcntl(clientFd, F_GETFL, 0);
-    if (flags == -1 || fcntl(clientFd, F_SETFL, flags | O_NONBLOCK) == -1)
-    {
-        std::cerr << "[Server] Échec de fcntl() sur le client." << std::endl;
-        close(clientFd);
-        return;
-    }
-
-    // Ajout dans la surveillance de poll()
-    struct pollfd clientPollFd;
-    clientPollFd.fd = clientFd;
-    clientPollFd.events = POLLIN;
-    clientPollFd.revents = 0;
-    this->_pollVec.push_back(clientPollFd);
-
-    // POINT D'ANCRAGE POUR TES AMIS :
-    // this->_clients[clientFd] = Client(clientFd);
-
-    std::cout << "[Server] Nouveau client connecté (FD: " << clientFd << ")" << std::endl;
-}
-
-void Server::_handleClientData(size_t index)
-{
-    char buffer[1024];
-    std::memset(buffer, 0, sizeof(buffer));
-
-    int clientFd = this->_pollVec[index].fd;
-    ssize_t bytesRead = recv(clientFd, buffer, sizeof(buffer) - 1, 0);
-
-    if (bytesRead <= 0)
-    {
-        // 0 = Déconnexion normale par le client ; < 0 = Erreur réseau
-        this->_disconnectClient(index);
-    }
-    else
-    {
-        std::string rawData(buffer);
-        std::cout << "[Client " << clientFd << "] Reçu : " << rawData;
-
-        // POINT D'ANCRAGE POUR TES AMIS :
-        // 1. Ajouter rawData au buffer interne de l'objet Client.
-        // 2. Vérifier la présence d'un délimiteur "\r\n" ou "\n".
-        // 3. Envoyer la ligne complète à votre parseur de commandes (NICK, USER, JOIN, etc.).
-    }
-}
-
-void Server::_disconnectClient(size_t index)
-{
-    int clientFd = this->_pollVec[index].fd;
-
-    std::cout << "[Server] Client déconnecté (FD: " << clientFd << ")" << std::endl;
-
-    close(clientFd);
-    this->_pollVec.erase(this->_pollVec.begin() + index);
-
-    // POINT D'ANCRAGE POUR TES AMIS :
-    // this->_clients.erase(clientFd);
-    // Retirer le client de tous les salons (_channels) où il était inscrit.
-}
-
-void Server::stop()
-{
-    this->_running = false;
 }
