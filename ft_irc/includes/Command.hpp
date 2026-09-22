2109157e87411d358e45d0878286b4a567b5b823
athamilc <athamilc@f2r4s15.paris.42.school>
2026-09-22 22:19:16 +0200

    quit2

--- ft_irc/includes/Command.hpp
+++ ft_irc/includes/Command.hpp
@@ -6,7 +6,7 @@
 /*   By: athamilc <athamilc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/22 19:07:48 by athamilc          #+#    #+#             */
-/*   Updated: 2026/09/22 17:28:49 by athamilc         ###   ########.fr       */
+/*   Updated: 2026/09/22 21:51:56 by athamilc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ class Command
 
     bool isValidNickname(const std::string& nickname);
     bool quit(Client& client, const std::string& reason);
-
+    bool privmsg(Client& client, const std::string& target, const std::string& message);
 };
 
 #endif
